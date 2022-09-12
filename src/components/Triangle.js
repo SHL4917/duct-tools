ccd3796225c51c941487e51af9729cea11a9f939
Siew <siewhan96@gmail.com>
2022-09-12 18:45:51 +0100

    Removed conflicting tailwind css

--- src/components/Triangle.js
+++ src/components/Triangle.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 const Triangle = (props) => {
-  let height = props.height ?? 5;
-  let width = 2 * height * 0.866;
-  //let triangleStyle = `w-0 h-0 border-x-transparent border-t-gray-300 border-x-[${parseInt(height)}px] border-t-[${parseInt(width)}px]`;
   let triangleStyle = `w-0 h-0 border-x-transparent border-x-[4px]`;
 
   if (props.up) {
